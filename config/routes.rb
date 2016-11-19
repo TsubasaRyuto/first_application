cd4a83d606152a2fbc20bb1ebbb6371e40b47a24
ryuto <tun_tnd@i.softbank.jp>
2016-11-19 15:40:39 +0900

    add routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, {format: 'json'} do
+    resources :users, except: [:index, :new, :edit, :show]
+    post '/login', to: 'sessions#login'
+    delete '/logout', to: 'sessions#destroy'
+  end
 end
