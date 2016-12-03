bab27113418226fdd97301e02edba160470b24f6
ryuto <tun_tnd@i.softbank.jp>
2016-12-03 09:50:30 +0900

    fixes config auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
+Rails.application.configure.middleware.use OmniAuth::Builder do
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], {
     strategy_class: OmniAuth::Strategies::Facebook,
-    provider_ignores_state: true,
+    provider_ignores_state: true
   }
 end
