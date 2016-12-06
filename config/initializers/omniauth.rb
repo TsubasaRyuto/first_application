5d3c1d0cde8492b3c3705f358ca8670c2b710fc5
ryuto <tun_tnd@i.softbank.jp>
2016-12-06 17:25:15 +0900

    fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-    :scope => 'email, name, nickname, email, gender, url, image_url', :info_fields => 'email'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
