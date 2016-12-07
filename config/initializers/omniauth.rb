7a7e37d97a3636cafeefc666fad0df82baa81db9
ryuto <tun_tnd@i.softbank.jp>
2016-12-08 00:34:23 +0900

    fixes omniauth facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], { provider_ignores_state: true }
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+  scope: "email", display: "popup",
+  client_options: {
+    site: 'https://graph.facebook.com/v2.8',
+    authorize_url: "https://www.facebook.com/v2.8/dialog/oauth"
+  },
+  local: "ja_JP",
+  info_fields: "email, name"
 end
