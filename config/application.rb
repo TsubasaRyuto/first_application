51489c03ea8eb51317cdb88ad0dfa17cf5dc1d48
ryuto <tun_tnd@i.softbank.jp>
2017-01-14 17:28:41 +0900

    fixes bugs

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module ObOgMatcher
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: false,
